867abb5fe6a9a108dcdf196b01c26201883bc3fb
nikita-pampushka <nikitapampushka@gmail.com>
2022-03-28 21:33:28 +0300

    Updated

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-let quantityOfOperations = prompt('Quantity of operations:');
+let quantityOfOperations = +prompt('Quantity of operations:');
 
-if (Number(quantityOfOperations) <= 0) {
+if (quantityOfOperations <= 0) {
     alert(`No actions needed...`);
 } else {
-    for (let i = 0; i < Number(quantityOfOperations); i++) {
-        operand1 = prompt('Operand 1:');
-        operator = prompt('Operator:');
-        operand2 = prompt('Operand 2:');
+    for (let i = 0; i < quantityOfOperations; i++) {
+        let operand1 = +prompt('Operand 1:');
+        let operator = prompt('Operator:');
+        let operand2 = +prompt('Operand 2:');
 
         if (operator === '+') {
-            result = Number(operand1) + Number(operand2);
+            result = operand1 + operand2;
             console.log(`Result: ${operand1} ${operator} ${operand2} = ${result}`);
         } else if (operator === '-') {
-            result = Number(operand1) - Number(operand2);
+            result = operand1 - operand2;
             console.log(`Result: ${operand1} ${operator} ${operand2} = ${result}`);
         } else if (operator === '*') {
-            result = Number(operand1) * Number(operand2);
+            result = operand1 * operand2;
             console.log(`Result: ${operand1} ${operator} ${operand2} = ${result}`);
         } else if (operator === '/') {
-            result = Number(operand1) / Number(operand2);
+            result = operand1 / operand2;
             console.log(`Result: ${operand1} ${operator} ${operand2} = ${result}`);
         } else {
             alert(`This operator does not exist!`);
